8c450bf20a0869f296ef3e922b3e15f39e94a3c6
Brendon <bbrendonshi@gmail.com>
2018-10-13 16:54:36 +1100

    edited routes

--- routes.py
+++ routes.py
@@ -9,24 +9,8 @@ from trade import *
 def index(id):
 	return render_template("index.html", id = id)
 
-@app.route("/trade/<id>", methods=["GET", "POST"])
-def trade(id):
-    #date, place, food
-    if request.method == "POST":
-        # updating the quantities of each food
-        counter = 1
-        while counter <= details.n_food:
-            buffer = "food"
-            buffer += counter
-            request.form.get('')
-            counter += 1
-        
-
-    return render_template("trade.html", details = details)
-
 @app.route("/addTrade/<int:id>", methods=["GET","POST"])
 def addTrade(id):
     if request.method == "POST":
-        #logic here
-        pass
+    	
     return render_template("addTrade.html", id=id)
